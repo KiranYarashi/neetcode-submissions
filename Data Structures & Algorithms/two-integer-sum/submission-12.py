a4f5a1ac73c08ad8ad356c9b2749f5d3f85bf962
Kiran Yarashi <99976796+KiranYarashi@users.noreply.github.com>
2026-08-02 19:55:55 +0530

    Add: two-integer-sum - submission-12

---
+++ Data Structures & Algorithms/two-integer-sum/submission-12.py
@@ -0,0 +1,14 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        myDict = {}  #store value and index
+
+        if len(nums) == 0:
+            return False
+
+        for i  in range(len(nums)):
+            duplicate = target - nums[i]  
+            if duplicate in myDict:
+                return [ myDict[duplicate], i]
+            myDict[nums[i]] = i
+
+        return []
